ece92d67396cbebf38f6eb7bd1f7608460e9987c
Italo Gonçales <italo.goncales@gmail.com>
2021-08-09 17:02:10 -0300

    Add all latest updates

--- src/routes.js
+++ src/routes.js
@@ -22,7 +22,7 @@ export default class App extends Component {
     render() {
         return (
             <NavigationContainer>
-                <Stack.Navigator initialRouteName="Main">
+                <Stack.Navigator initialRouteName="Login">
                     <Stack.Screen
                         name="Login"
                         component={Login}
